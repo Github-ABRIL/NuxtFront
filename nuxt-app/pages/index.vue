27686a3c02b33a77cd40d42673a4a86e948028eb
miguelmurga <miguelmurgaguevarahotmail.com>
2024-04-20 18:31:03 -0600

    Implemente (Hola mundo)

---
+++ nuxt-app/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ mensaje }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mensaje: '¡Hola, mundo!',
+    };
+  },
+};
+</script>
